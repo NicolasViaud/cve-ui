bc66af24568412533bd8ef24f7843754cbcd4b7c
Nicolas Viaud <nicolas.viaud84@gmail.com>
2022-07-10 22:00:44 +0200

    add pages

---
+++ src/app/components/page-not-found/page-not-found.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PageNotFoundComponent } from './page-not-found.component';
+
+
+
+@NgModule({
+  declarations: [
+    PageNotFoundComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PageNotFoundModule { }
